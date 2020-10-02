51d4e51217c3af2ede57ea5ff62289ce24f4a655
sun-xiaoxu <sunxiaoxu0729@gmail.com>
2020-10-02 23:22:09 +0800

    是

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
-set -e
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-hugo
-cd public
-git add .
-
-
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
-
-# 推送到sun-xiaoxu.github.io仓库中
-git push origin master
+#Copyright 2018 Google LLC
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#
+rm -rf public/
+HUGO_ENV="production" hugo --gc || exit 1
+s3deploy -source=public/ -region=eu-west-1 -bucket=bep.is -distribution-id=E8OKNT7W9ZYZ2 -path temp/td
